cd06e5f4f4d94af9e9fa8e7953254229c49b2407
illia.rybas <illia.rybas@tatemgames.com>
2021-01-15 17:11:39 +0200

    Change orbital movement logic

--- Planetarity-v1.2/Assets/Scripts/OrbitalMovement.cs
+++ Planetarity-v1.2/Assets/Scripts/OrbitalMovement.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Planetarity
+﻿namespace Planetarity
 {
+    using UnityEngine;
+
     public class OrbitalMovement : MonoBehaviour
     {
-        [SerializeField] private Transform target;
+        [SerializeField] private float speed = 90;
 
-        [SerializeField] private float speed;
+        [SerializeField] private float radius = 2;
+
+        private float _currentAngle;
 
         private void Update()
         {
@@ -17,7 +17,12 @@ namespace Planetarity
 
         private void Move()
         {
-            transform.RotateAround(target.transform.position, Vector3.forward, speed * Time.deltaTime);
+            _currentAngle += speed * Mathf.Deg2Rad * Time.deltaTime;
+
+            var x = Mathf.Cos(_currentAngle);
+            var y = Mathf.Sin(_currentAngle);
+
+            transform.position = new Vector2(x, y) * radius;
         }
     }
 }
