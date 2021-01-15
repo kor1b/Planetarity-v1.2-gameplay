b228112dda0a4a20a24d854c623f7d63b4baa406
illia.rybas <illia.rybas@tatemgames.com>
2021-01-15 14:23:06 +0200

    Simple rotate around Sun

---
+++ Planetarity-v1.2/Assets/Scripts/OrbitalMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Planetarity
+{
+    public class OrbitalMovement : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+
+        [SerializeField] private float speed;
+
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            transform.RotateAround(target.transform.position, Vector3.forward, speed * Time.deltaTime);
+        }
+    }
+}
