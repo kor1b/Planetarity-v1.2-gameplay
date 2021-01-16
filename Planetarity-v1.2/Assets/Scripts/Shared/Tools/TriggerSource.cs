97a50371255312301e6a3c06f8fd21a057422355
illia.rybas <illia.rybas@tatemgames.com>
2021-01-16 11:00:11 +0200

    Trigger source

---
+++ Planetarity-v1.2/Assets/Scripts/Shared/Tools/TriggerSource.cs
@@ -0,0 +1,30 @@
+﻿namespace Planetarity
+{
+    using System;
+    using UnityEngine;
+
+    [RequireComponent(typeof(Collider))]
+    public class TriggerSource : MonoBehaviour
+    {
+        public Action<Collider> OnEnter;
+
+        public Action<Collider> OnStay;
+        
+        public Action<Collider> OnExit;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            OnEnter?.Invoke(other);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            OnStay?.Invoke(other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            OnExit?.Invoke(other);
+        }
+    }
+}
