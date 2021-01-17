d8fd618036156e8f1b3fa6e96f6a30a310d62bb3
illia.rybas <illia.rybas@tatemgames.com>
2021-01-17 12:39:48 +0200

    Pause button

---
+++ Planetarity-v1.2/Assets/Scripts/GameFlow/Pause.cs
@@ -0,0 +1,29 @@
+﻿namespace Planetarity
+{
+    using UnityEngine;
+
+    public class Pause : MonoBehaviour
+    {
+        private bool isPaused = false;
+
+        public void TogglePause()
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+
+        private void PauseGame()
+        {
+            isPaused = true;
+            Time.timeScale = 0;
+        }
+
+        private void ResumeGame()
+        {
+            isPaused = false;
+            Time.timeScale = 1;
+        }
+    }
+}
